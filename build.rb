e9ba65526ddc22d05b2c90801c72db0048a6370a
Juha-Jarmo Heinonen <o@sorsacode.com>
2011-06-01 22:31:21 +0300

    the build script now supports multiple docs (any valid documentation bundles in the doc directory), it also implements a custom imagebox extension to markdown

--- build.rb
+++ build.rb
@@ -20,8 +20,9 @@
 
 require 'rubygems'
 require 'bluecloth'
+require 'yaml'
 
-class BuildDocs
+class BuildDoc
   def error( text )
     puts
     puts "ERROR: #{text}"
@@ -33,10 +34,16 @@ class BuildDocs
     error("Missing template: #{tmpl_path}") unless File.exist?( tmpl_path )
     File.read( tmpl_path )
   end
-  def read_order
-    # placeholder, will read actual config file later
-    { 'spin1' => [ 'links', 'intro', 'index' ] }[@proj_name]
-
+  def read_config
+    config_path = File.expand_path( 'config.yaml', @src_path )
+    @config = YAML.load_file( config_path )
+  end
+  def md_order
+    @config['order']
+  end
+  def read_css
+    css_path = File.expand_path( 'template.css', @src_path )
+    @css = File.read( css_path )
   end
   def read_markdown
     Dir.entries( @src_path ).each do |md_file|
@@ -50,53 +57,96 @@ class BuildDocs
       @src[md_name] = md_src
     end
   end
-  def write_html( content )
+  def write_html
     File.open( @dst_path, 'w' ) do |f|
-      f.write( content )
+      f.write( @html )
+    end
+  end
+  def build_img_box( width, height, src, title, md_src )
+    html = BlueCloth.new( md_src ).to_html
+    f_width = 440
+    if width > f_width
+      f_height = ((height/width.to_f)*f_width).round
+      img_src = %{<img src="#{src}" width=#{f_width} height=#{f_height} title="#{title}" alt="#{title}">}
+    else
+      f_height = height
+      img_src = %{<img src="#{src}" width=#{width} height=#{height} title="#{title}" alt="#{title}">}
+    end
+    outp = %{
+<div class=imagebox style="min-height:#{f_height+20}px">
+  <a class=pict href="#{src}">
+    #{img_src}
+    <div class=pict_title>#{title}</div>
+  </a>
+  <div class=text>
+#{html}
+  </div>
+</div>
+}
+  end
+  def init_re
+    @img_box_re = /\/-- ([0-9]+)x([0-9]+) (.*?) \"(.*?)\"(.*?)--\//m
+  end
+  def extra_md
+    @md_src.gsub!(@img_box_re) { build_img_box( $1.to_i, $2.to_i, $3, $4, $5 ) }
+  end
+  def process_template
+    extra_md
+    html_body = BlueCloth.new( @md_src ).to_html
+    @html = html_tmpl.gsub(
+      '__TITLE__', @title
+    ).gsub(
+      '__HTML__',  html_body
+    ).gsub(
+      '__CSS__', @css
+    )
+  end
+  def set_title
+    if @config.has_key?('title')
+      @title = @config['title']
+    else
+      @title = @proj_name.capitalize
     end
   end
   def generate_html
     md_src = []
-    read_order.each do |md_name|
+    md_order.each do |md_name|
       error("Missing reference: #{md_name}") unless @src.has_key?( md_name )
+      md_src.push( %{<br id="#{md_name}">})
       md_src.push( @src[md_name] )
     end
-    html = BlueCloth.new( md_src.join("\n\n") ).to_html
-    write_html( html_tmpl.gsub( '__HTML__', html ) )
+    @md_src = md_src.join("\n\n")
+    set_title
+    read_css
+    process_template
+    write_html
   end
   def initialize( src_path, dst_path )
+    init_re
     error("Missing source path: #{src_path}") unless File.exist?( src_path )
     unless File.exist?( dst_path )
-      puts
-      puts 'Destination path missing, creating:'
-      puts dst_path
-      puts
       Dir.mkdir( dst_path )
     end
     @src_path = src_path
     @proj_name = File.split( src_path )[1]
     @dst_path = File.expand_path( @proj_name+'.html', dst_path )
+    read_config
     @src  = {}
-    puts
-    puts 'Reading Markdown from:'
-    puts @src_path
-    puts
     read_markdown
-    puts
-    puts 'Writing html into:'
-    puts @dst_path
-    puts
     generate_html
   end
 end
-
-self_path = File.split( File.expand_path( __FILE__ ) ).first
-src_path = File.expand_path( 'spin1', self_path )
-dst_path = File.expand_path( 'html', self_path )
-puts
-puts 'Initializing FreeEMS Documentation Build'
-puts
-BuildDocs.new( src_path, dst_path )
-puts
-puts 'FreeEMS Documentation Build completed without errors.'
-puts
+class BuildDocs
+  def initialize( base_path )
+    dst_path = File.expand_path( 'html', base_path )
+    doc_path = File.expand_path( 'doc', base_path )
+    Dir.entries( doc_path ).each do |proj_name|
+      next if proj_name.start_with?('.')
+      src_path = File.expand_path( proj_name, doc_path )
+      next unless File.directory?( src_path )
+      puts proj_name
+      BuildDoc.new( src_path, dst_path )
+    end
+  end
+end
+BuildDocs.new( File.split( File.expand_path( __FILE__ ) ).first )
